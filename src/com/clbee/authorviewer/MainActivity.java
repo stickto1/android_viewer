93fcb175774366d387bb98a4bd28062ebda08d71
ohjeong <ksfaly@clbee.com>
2013-02-05 17:22:26 +0900

    add AnimationUtil

--- src/com/clbee/authorviewer/MainActivity.java
+++ src/com/clbee/authorviewer/MainActivity.java
@@ -1,8 +1,14 @@
 package com.clbee.authorviewer;
 
+import com.kic.basicAni.BasicAnimation;
+
+import android.R.interpolator;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -21,9 +27,13 @@ public class MainActivity extends Activity {
 	}
 
 	private void setText() {
-//		captionView.setText(getResources().getString(R.string.hello_world));
+		// captionView.setText(getResources().getString(R.string.hello_world));
 		captionView.setText("바깠어용");
+		BasicAnimation.TranslateAni(Animation.RELATIVE_TO_SELF, 0,
+				Animation.RELATIVE_TO_SELF, 2, Animation.RELATIVE_TO_PARENT, 0,
+				Animation.RELATIVE_TO_PARENT, 0, 1000);
+		BasicAnimation.setAniOption(true, 3, 0, new LinearInterpolator(), 0);
+		BasicAnimation.startAnimation(captionView);
 	}
-	
-	
+
 }
