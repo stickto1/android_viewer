196d8f6900b62ebc473f2a775ba448cebabad7fe
ohjeong <ksfaly@clbee.com>
2013-02-05 15:57:41 +0900

    Athoring 1 - kic

---
+++ src/com/clbee/authorviewer/MainActivity.java
@@ -0,0 +1,26 @@
+package com.clbee.authorviewer;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private TextView captionView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findView();
+		setText();
+	}
+
+	private void findView() {
+		captionView = (TextView) findViewById(R.id.captionView);
+	}
+
+	private void setText() {
+		captionView.setText(getResources().getString(R.string.hello_world));
+	}
+}
