d0141565a1da1a272f4d8f9a96e46f516190a90d
ohjeong <ksfaly@clbee.com>
2013-03-05 18:01:19 +0900

    파서추가

--- gen/com/clbee/authorviewer/R.java
+++ gen/com/clbee/authorviewer/R.java
@@ -15,18 +15,20 @@ public final class R {
     }
     public static final class id {
         public static final int captionView=0x7f070000;
-        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
-    public static final class menu {
-        public static final int activity_main=0x7f060000;
+    public static final class raw {
+        public static final int cover=0x7f040000;
+        public static final int docu=0x7f040001;
+        public static final int test=0x7f040002;
+        public static final int view=0x7f040003;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
     }
     public static final class style {
         /** 
@@ -38,22 +40,11 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/clbee/authorviewer/MainActivity.java
+++ src/com/clbee/authorviewer/MainActivity.java
@@ -1,10 +1,16 @@
 package com.clbee.authorviewer;
 
-import com.kic.basicAni.BasicAnimation;
+import java.util.ArrayList;
 
-import android.R.interpolator;
+import com.google.gson.Gson;
+import com.kic.basicAnimation.BasicAnimation;
+import com.kic.util.JsonUtil;
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.Log;
 import android.view.Menu;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
@@ -14,7 +20,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements AnimationListener {
-	private ImageView captionView;
+	private ImageView mTargetView;
 	private BasicAnimation basicAni;
 
 	@Override
@@ -26,37 +32,49 @@ public class MainActivity extends Activity implements AnimationListener {
 	}
 
 	private void findView() {
-		captionView = (ImageView) findViewById(R.id.captionView);
+		mTargetView = (ImageView) findViewById(R.id.captionView);
 	}
 
 	private void setText() {
+		JsonUtil jsonUtil = new JsonUtil();
+//		String jsonResult = jsonUtil.getJsonStringFromFile("docu", "raw", this);
+//		String jsonResult = jsonUtil.getJsonStringFromFile("view", "raw", this);
+		String jsonResult = jsonUtil.getJsonStringFromFile("test", "raw", this);
+		jsonUtil.jsonParser(jsonResult);
+//		TestKeyName keyName = jsonUtil.jsonStringToClass(jsonResult);
+//		jsonUtil.jsonParser(jsonResult);
+		
+		
+		
+		
+		
 		basicAni = new BasicAnimation();
-		basicAni.ScaleAni(0, 5, 0, 5, Animation.RELATIVE_TO_SELF, 0.5f,
-				Animation.RELATIVE_TO_SELF, 0.5f, 3000);
+		Animation scaleAnimation = basicAni.ScaleAni(0, 10, 0, 10, Animation.ABSOLUTE, 0.5f,
+				Animation.ABSOLUTE, 0.5f, 3000);
 		basicAni.setAniOption(true, 0, 0, new LinearInterpolator(), 0);
-		basicAni.addAniList();
+		basicAni.addAnimationList(scaleAnimation);
 
-		basicAni.TranslateAni(Animation.ABSOLUTE, 0,
+		Animation translateAnimation = basicAni.TranslateAni(Animation.ABSOLUTE, 0,
 				Animation.ABSOLUTE, 200, Animation.ABSOLUTE, 0,
 				Animation.ABSOLUTE, 200, 2000);
 		basicAni.setAniOption(true, 0, 0, new LinearInterpolator(), 0);
-		basicAni.addAniList();
+		basicAni.addAnimationList(translateAnimation);
 
 
-		basicAni.ScaleAni(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0.5f,
+		Animation scaleAnimaion = basicAni.ScaleAni(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0.5f,
 				Animation.RELATIVE_TO_SELF, 0.5f, 3000);
-		basicAni.addAniList();
+		basicAni.addAnimationList(scaleAnimaion);
 		
-		basicAni.TranslateAni(Animation.ABSOLUTE, 200,
+		Animation transAnimation2 = basicAni.TranslateAni(Animation.ABSOLUTE, 200,
 				Animation.ABSOLUTE, 200, Animation.ABSOLUTE, 200,
 				Animation.ABSOLUTE, 400, 3000);
 		basicAni.setAniOption(true, 0, 0, new LinearInterpolator(), 0);
-		basicAni.addAniList();
+		basicAni.addAnimationList(transAnimation2);
 		
-		basicAni.RotateAni(0, -180, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f, 3000);
-		basicAni.addAniList();
+		Animation rotateAnimation = basicAni.RotateAni(0, -180, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f, 3000);
+		basicAni.addAnimationList(rotateAnimation);
 		
-		basicAni.startAnimation(captionView, 0);
+		basicAni.startAnimation(mTargetView, 1);
 		basicAni.getAni(0).setAnimationListener(this);
 		basicAni.getAni(1).setAnimationListener(this);
 		basicAni.getAni(2).setAnimationListener(this);
@@ -71,16 +89,29 @@ public class MainActivity extends Activity implements AnimationListener {
 	@Override
 	public void onAnimationEnd(Animation animation) {
 		// TODO Auto-generated method stub
+//		if (animation.equals(basicAni.getAni(0))) {
+//			basicAni.startAnimation(mTargetView, 1);
+//		} else if (animation.equals(basicAni.getAni(1))) {
+//			basicAni.startAnimation(mTargetView, 2);
+//		} else if (animation.equals(basicAni.getAni(2))) {
+//			basicAni.startAnimation(mTargetView, 3);
+//		} else if (animation.equals(basicAni.getAni(3))) {
+//			basicAni.startAnimation(mTargetView, 4);
+//		} else if (animation.equals(basicAni.getAni(4))) {
+//			basicAni.startAnimation(mTargetView, 0);
+//		}
+		
+		
 		if (animation.equals(basicAni.getAni(0))) {
-			basicAni.startAnimation(captionView, 1);
+			basicAni.startAnimation(mTargetView, 1);
 		} else if (animation.equals(basicAni.getAni(1))) {
-			basicAni.startAnimation(captionView, 2);
+			basicAni.startAnimation(mTargetView, 2);
 		} else if (animation.equals(basicAni.getAni(2))) {
-			basicAni.startAnimation(captionView, 3);
+			basicAni.startAnimation(mTargetView, 3);
 		} else if (animation.equals(basicAni.getAni(3))) {
-			basicAni.startAnimation(captionView, 4);
+			basicAni.startAnimation(mTargetView, 4);
 		} else if (animation.equals(basicAni.getAni(4))) {
-			basicAni.startAnimation(captionView, 0);
+			basicAni.startAnimation(mTargetView, 0);
 		}
 
 	}
